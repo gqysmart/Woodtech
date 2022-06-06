b21f01e50e0b47622768e8da7feb5382b471d33e
qiyunge <157900653@qq.com>
2022-06-06 16:28:47 +0800

    first commit

---
+++ src/pages/test.js
@@ -0,0 +1,10 @@
+import * as React from "react"
+import { StaticImage } from "gatsby-plugin-image"
+
+const test = () => (
+  <h1>
+    <StaticImage src="../images/example.png"></StaticImage>
+  </h1>
+)
+
+export default test
