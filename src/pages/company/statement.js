69da61ffc1b306b6708eab8cc9e9df9967d266ad
qiyunge <157900653@qq.com>
2022-06-08 00:32:50 +0800

    build;modify readme.md

---
+++ src/pages/company/statement.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const statement = () => {
+  return <div>statement</div>
+}
+
+export default statement
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        main: " Georgia, -apple-system, 'Nimbus Roman No9 L', 'PingFang SC', 'Hiragino Sans GB', 'Noto Serif SC', 'Microsoft Yahei', 'WenQuanYi Micro Hei', 'ST Heiti', sans-serif",
+        head: "'Josefin Sans', -apple-system, 'PingFang SC', 'Hiragino Sans GB', 'Microsoft Yahei', 'WenQuanYi Micro Hei', 'ST Heiti', sans-serif",
+      },
+    },
   },
-  plugins: [
-    'gatsby-plugin-postcss',
-  ],
+  plugins: ["gatsby-plugin-postcss"],
 }
